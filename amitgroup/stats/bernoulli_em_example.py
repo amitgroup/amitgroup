6cc1f0860a230e3798793d4645e01f5d9a227737
markstoehr <stoehr.mark@gmail.com>
2012-07-12 18:00:21 -0500

    added in new bernoulli examples

---
+++ amitgroup/stats/bernoulli_em_example.py
@@ -0,0 +1,31 @@
+import bernoulli_em
+import numpy as np
+
+# generate synthetic data
+num_templates = 3
+template_size = (20,20)
+templates = np.zeros((num_templates,
+                      template_size[0],
+                      template_size[1]))
+for T in templates:
+    x = np.random.randint(template_size[0]-4)
+    y = np.random.randint(template_size[1]-4)
+    T[x:x+4,
+      y:y+4] = .95
+    T = np.maximum(.05,T)
+
+for T in templates:
+    T = np.maximum(.05,T)
+
+
+num_data = 100
+XS = np.zeros((num_data,template_size[0],template_size[1]))
+
+for X in XS:
+    S = np.random.rand(template_size[0],template_size[1])
+    X[S < templates[np.random.randint(num_templates)]] = 1.
+
+
+bm = bernoulli_em.Bernoulli_Mixture(3,XS)
+bm.run_EM(.0001)
+
