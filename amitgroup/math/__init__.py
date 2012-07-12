90e06aaa9c286a1e3ac3dace62590bd33b677ed6
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-07-12 15:47:33 -0500

    Better Python 3 support.

--- amitgroup/math/__init__.py
+++ amitgroup/math/__init__.py
@@ -1,10 +1,12 @@
 
+from __future__ import absolute_import
+
 try:
-    from interp2d import *
+    from .interp2d import *
 except ImportError:
-    from interp2d_pure import *
+    from .interp2d_pure import *
     print("Warning: No Cython! Please compile amitgroup.")
 
-from blur import blur_image
+from .blur import blur_image
 
 __all__ = ['interp2d', 'blur_image']
