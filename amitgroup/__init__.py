71dac380090b1e99a084b50801db055bd2c7dae3
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-07-24 14:50:07 -0500

    All modules are loaded by default.

--- amitgroup/__init__.py
+++ amitgroup/__init__.py
@@ -2,7 +2,13 @@ from __future__ import absolute_import
 
 # Load the following modules by default
 from .core import *
+
+# Lazy load these?
 import amitgroup.io
+import amitgroup.features
+import amitgroup.ml
+import amitgroup.stats
+import amitgroup.plot
 
 VERSION = (0,0,0)
 ISRELEASED  = False
