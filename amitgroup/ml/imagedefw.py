985f18ed776c465c1917d532e25ff45c56ea38e0
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-08-03 12:07:19 -0500

    Imagedef revamp cleaned up.
    - Also added support for non-square images.

--- amitgroup/ml/imagedefw.py
+++ amitgroup/ml/imagedefw.py
@@ -11,7 +11,7 @@ def _powerof2(v):
     return (v & (v-1)) == 0 
 
 def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
-             calc_costs=False, wavelet='db2', \
+             wavelet='db2', \
              max_iterations_per_level=1000, start_level=1, stepsize_scale_factor=1.0):
     """
     Deforms an a prototype image `F` into a data image `I` using a Daubechies wavelet basis and maximum a posteriori. 
@@ -19,9 +19,9 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
     Parameters
     ----------
     F : ndarray
-        Prototype image. Array of shape ``(L, L)`` with normalized intensitites. So far, ``L`` has to be a power of two.
+        Prototype image. Array of shape ``(W, H)`` with normalized intensitites. Both `W` and `H` must be powers of two.
     I : ndarray
-        Data image that the prototype will try to deform into. Array of shape ``(L, L)``. 
+        Data image that the prototype will try to deform into. Array of shape ``(W, H)``. 
     last_level : int
         Coefficient depth limit. If `None`, then naturally bounded by image size. 
         A higher level will allow for finer deformations, but incur a computational overhead.
@@ -29,8 +29,6 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
         Determines the weight of the prior as opposed to the likelihood. (arbitrarily proportional to the ratio of the inverse variance of the gaussian deformations of the prior and the likelihood). Reduce this value if you want more deformations.
     rho : float
         Determines the penalty of more granular coefficients. Increase to smoothen. Must be strictly positive.
-    calc_costs : bool
-        If True, then ``info`` will contain `logprior` and `loglikelihood`. The cost function `J` is the negative sum of these two. 
     first_level : int
         First coarse-to-fine coefficient level. Defaults to 1 and generally does not needed to be fiddle with. 
     stepsize_scale_factor : float
@@ -42,10 +40,10 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
         The deformation in the form of a :class:`DisplacementField`. 
     info : dict
         Dictionary with info:
-         * `iterations`: Total number of iterations, ``N``.
+         * `iterations`: Total number of iterations, `N`.
          * `iterations_per_level`: Iterations per coarseness level.
-         * `logpriors`: The value of the log-prior for each iteration. Array of length ``N``.
-         * `loglikelihoods`: The value of the log-likelihood for each iteration. Array of length ``N``.
+         * `logpriors`: The value of the log-prior for each iteration. Array of length `N`.
+         * `loglikelihoods`: The value of the log-likelihood for each iteration. Array of length `N`.
          * `costs`: The cost function over time (the negative sum of logprior and loglikelihood)
     """
     # Speed this up. How?
@@ -69,32 +67,21 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
     >>> ag.plot.deformation(F, I, imdef)
      
     """
-    """Old stuff:
-    stepsize : float or None
-        Gradient descent step size. If `None`, then the stepsize will be inferred by something along the lines of Newton's method.
-    """
     assert rho > 0, "Parameter rho must be strictly positive"
     assert len(F.shape) == 2 and len(I.shape) == 2, "Images must be 2D ndarrays"
     assert _powerof2(F.shape[0]) and _powerof2(I.shape[1]), "Image sides must be powers of 2"
     assert F.shape == I.shape, "Images must have the same shape"
 
-    # This should work, but is very wonky. Needs more testing.
-    assert F.shape[0] == F.shape[1], "Sides must be the same length... For now. Will fix."
-
     # Shift the penalty value, as to make 1.0 a reasonable value.
-    penalty_adjusted = penalty
-
     logpriors = []
     loglikelihoods = []
 
     dx = 1/np.prod(F.shape)
 
+    # Notice that the image is always considered to lie in the range [0, 1] on both axes.
     delF = np.gradient(F, 1/F.shape[0], 1/F.shape[1])
-    # Normalize since the image covers the square around [0, 1].
     
     imdef = ag.util.DisplacementFieldWavelet(F.shape, penalty=penalty, rho=rho, wavelet=wavelet)
-    
-    #imdef.u[0,0,0,0,0] = 3.528
     
     x, y = imdef.meshgrid()
 
@@ -115,8 +102,6 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
             num_iterations += 1
             # 2.
         
-            #print "u0 = ", imdef.u[0,0,0,0,0]
-
             # Calculate deformed xs
             Ux, Uy = imdef.deform_map(x, y)
             z0 = x + Ux
@@ -125,7 +110,6 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
             # Interpolate F at zs
             Fzs = ag.util.interp2d(z0, z1, F)
             #Fzs = imdef.deform(F)
-            #np.testing.assert_array_almost_equal(ag.util.interp2d(z0, z1, F), Fzs)
 
             # Interpolate delF at zs 
             delFzs = np.empty((2,) + F.shape) 
@@ -137,38 +121,22 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
 
             # Calculate (cost) 
     
-            #sigma = np.sqrt(dx) / np.sqrt(2)
-            #sigma2 = sigma**2
-            # Technicall we should have dx and sigma involved here,
+            # Technicall we should have a sigma involved here,
             # but we are letting them be absorbed into the ratio
-            # between prior and loglikelihood.
-            loglikelihood = -(terms**2).sum() * dx / 2.0#/2.0# * dx / (2 * sigma2)
-            if calc_costs:
-                logprior = imdef.logprior(a)
-                logpriors.append(logprior)
+            # between prior and loglikelihood, which we call penalty.
+            loglikelihood = -(terms**2).sum() * dx / 2.0
+            logprior = imdef.logprior(a)
+            logpriors.append(logprior)
 
-                loglikelihoods.append(loglikelihood)
-        
-                if __debug__:# and loop_inner%10 == 0:
-                    ag.info("cost = {0} (prior: {1}, llh: {2}) [max U: {3}".format(-logprior-loglikelihood, logprior, loglikelihood, (imdef.lmbks * imdef.u**2).max()))
+            loglikelihoods.append(loglikelihood)
+    
+            if __debug__:# and loop_inner%10 == 0:
+                ag.info("cost = {0} (prior: {1}, llh: {2}) [max U: {3}".format(-logprior-loglikelihood, logprior, loglikelihood, (imdef.lmbks * imdef.u**2).max()))
 
             cost = -logprior - loglikelihood
             
-            # Check termination
-            #if cost > last_cost:
-            #    adjust *= 2.0
-            #    #ag.warning("The cost increased from {0} to {1}.".format(last_cost, cost))
-            #elif adjust > 1:
-            #    adjust /= 2.0
-
-            #print np.fabs(loglikelihood - last_loglikelihood)/last_loglikelihood
             if math.fabs(cost - last_cost)/last_cost < tol and loop_inner > 5:
                 break
-
-            #print 'cost', cost 
-            #print 'llh', loglikelihood
-            #print 'lprior', logprior
-            #print np.exp(loglikelihood)
 
             last_cost = cost
             last_loglikelihood = loglikelihood
@@ -179,13 +147,7 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
 
                 # This is the value of the psi, which turns out to be a good bound for this purpose
                 M = dx 
-
                 T = a**2 * M * delFzs2.sum() * dx + imdef.sum_of_coefficients(a)
-
-                if 1:
-                    print 'delFzs.mean()', delFzs.min(), delFzs.max()
-                    print a**2 * M * delFzs2.sum() * dx, imdef.sum_of_coefficients(a)
-                    print("1/T = {0} ({1})".format(1/T, T))
 
                 dt = stepsize_scale_factor/T
 
@@ -195,13 +157,6 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
             for q in range(2):
                 W[q] = delFzs[q] * terms
 
-            #import pywt
-            #u = pywt.wavedec(W[0][:,0], 'db1', mode='per')
-            #for uu in u[1:]:
-            #    uu[:] = 0.0 
-            #print "SHIFT", pywt.waverec(u, 'db1', mode='per')[0]
-            #print W[0][:,0].mean()
-        
             if 0:
                 import pylab as plt
                 print delFzs[0].shape
@@ -225,11 +180,10 @@ def imagedef(F, I, last_level=None, penalty=1.0, rho=2.0, tol=0.001, \
     info = {}
     info['iterations_per_level'] = iterations_per_level
     info['iterations'] = sum(iterations_per_level) 
-    if calc_costs:
-        logpriors = np.asarray(logpriors)
-        loglikelihoods = np.asarray(loglikelihoods)
-        info['logpriors'] = logpriors
-        info['loglikelihoods'] = loglikelihoods
-        info['costs'] = -logpriors - loglikelihoods
+    logpriors = np.asarray(logpriors)
+    loglikelihoods = np.asarray(loglikelihoods)
+    info['logpriors'] = logpriors
+    info['loglikelihoods'] = loglikelihoods
+    info['costs'] = -logpriors - loglikelihoods
 
-    return imdef, info 
+    return imdef, info
--- amitgroup/util/displacement_field_wavelet.py
+++ amitgroup/util/displacement_field_wavelet.py
@@ -8,34 +8,38 @@ from copy import deepcopy
 from .displacement_field import DisplacementField
 from .interp2d import interp2d
 
+def _levels2shape(levelshape, levels, level=np.inf):
+    level = min(level, levels)
+    return tuple([2**(max(0, level + levelshape[q] - levels - 1)) for q in range(2)])
+
 #TODO Move somewhere else, so as not to clog up the space
 # before the class.
-def _pywt2array(coefficients, scriptNs, maxL=np.inf):
-    L = len(scriptNs)#len(coefficients)
-    N = scriptNs[-1]#len(coefficients[-1][0])
-    new_u = np.zeros((L, 3, N, N))
+def _pywt2array(coefficients, L, levelshape, maxL=np.inf):
+    shape = _levels2shape(levelshape, L)
+    new_u = np.zeros((L, 3) + shape)
     for i in range(min(maxL, L)):
         if i == 0:
             Nx, Ny = coefficients[i].shape
-            assert Nx == scriptNs[i]
+            #assert Nx == scriptNs[i]
             new_u[i,0,:Nx,:Ny] = coefficients[i]
         else:
             for alpha in range(3):
                 Nx, Ny = coefficients[i][alpha].shape
-                assert Nx == scriptNs[i]
+                #assert Nx == scriptNs[i]
                 new_u[i,alpha,:Nx,:Ny] = coefficients[i][alpha]
                 
     return new_u
 
-def _array2pywt(coefficients, scriptNs):
+def _array2pywt(coefficients, levelshape, levels):
     new_u = []
-    for i, N in enumerate(scriptNs): 
-        if i == 0:
-            new_u.append(coefficients[i,0,:N,:N])
+    for level in range(levels+1): 
+        N, M = _levels2shape(levelshape, levels, level)
+        if level == 0:
+            new_u.append(coefficients[level,0,:N,:M])
         else:
             als = []
             for alpha in range(3):
-                als.append(coefficients[i,alpha,:N,:N])
+                als.append(coefficients[level,alpha,:N,:M])
             new_u.append(tuple(als))
     return new_u
 
@@ -69,7 +73,8 @@ class DisplacementFieldWavelet(DisplacementField):
         self.prepare_shape()
         self.rho = rho 
         biggest = self.scriptNs[-1]        
-        self.ushape = (2, self.levels+1, 3, biggest, biggest)
+        coefshape = _levels2shape(self.levelshape, self.levels)
+        self.ushape = (2, self.levels+1, 3) + coefshape
         # We divide the penalty, since the raw penalty is the ratio
         # of the variance between the coefficients and the loglikelihood.
         # It is more natural to want the variance between how much the 
@@ -91,19 +96,21 @@ class DisplacementFieldWavelet(DisplacementField):
         # Which ones are used? Create a mask
         mask = np.ones(self.ushape)
         for level in range(self.levels+1):
-            N = 2**(max(0, level-1))
+            N, M = _levels2shape(self.levelshape, self.levels, level)
             if level == 0:
-                mask[:,level,0,:N,:N] = 0 
+                mask[:,level,0,:N,:M] = 0 
             else:
-                mask[:,level,:,:N,:N] = 0 
+                mask[:,level,:,:N,:M] = 0 
 
         self.lmbks = np.ma.array(values, mask=mask)
         self.u = np.ma.array(np.zeros(self.ushape), mask=mask)
 
     def prepare_shape(self):
         #self.levels = len(pywt.wavedec(range(shape[0]), wl)) - 1
-        self.levels = int(np.log2(self.shape[0]))
-        self.scriptNs = map(len, pywt.wavedec(range(self.shape[0]), self.wavelet, level=self.levels, mode=self.mode))
+        side = max(self.shape)
+        self.levels = int(np.log2(side))
+        self.levelshape = tuple(map(int, map(np.log2, self.shape)))
+        self.scriptNs = map(len, pywt.wavedec(range(side), self.wavelet, level=self.levels, mode=self.mode))
 
     def _deformed_x(self, x0, x1):
         Ux0, Ux1 = self.deform_map(x0, x1)
@@ -114,8 +121,9 @@ class DisplacementFieldWavelet(DisplacementField):
         # TODO: Do waverec2 with cutoff coefficients and then patch it up with
         # linear interpolation instead! Should give comparable results, at least
         # for db2.
-        defx0 = pywt.waverec2(_array2pywt(self.u[0], self.scriptNs), self.wavelet, mode=self.mode) 
-        defx1 = pywt.waverec2(_array2pywt(self.u[1], self.scriptNs), self.wavelet, mode=self.mode)
+    
+        defx0 = pywt.waverec2(_array2pywt(self.u[0], self.levelshape, self.levels), self.wavelet, mode=self.mode) 
+        defx1 = pywt.waverec2(_array2pywt(self.u[1], self.levelshape, self.levels), self.wavelet, mode=self.mode)
 
         # Interpolated defx at xs 
         if x.shape == defx0.shape:
@@ -147,9 +155,9 @@ class DisplacementFieldWavelet(DisplacementField):
         Reestimation step for training the deformation. 
         """
         vqks = np.asarray([
-            _pywt2array(pywt.wavedec2(W[q], self.wavelet, mode=self.mode, level=self.levels), self.scriptNs, level) for q in range(2)
-        ]) / np.prod(self.shape) # Notice this adjustment of the values
-    
+            _pywt2array(pywt.wavedec2(W[q], self.wavelet, mode=self.mode, level=self.levels), len(self.scriptNs), self.levelshape, level) for q in range(2)
+        ]) / 4**self.levels  #/ np.prod(self.shape) # Notice this adjustment of the values
+
         self.u -= stepsize * (self.lmbks * self.u + vqks)
 
     def sum_of_coefficients(self, levels=None):
