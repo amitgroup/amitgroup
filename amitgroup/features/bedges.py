edb6c7aa3bd9a266f8ece32e9c98b6ce521b1e75
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-08-15 20:40:56 -0500

    Updates to bedges and load_mnist.
    - Option to return edge at shallow level, instead of deepest. This will return (8, rows, cols) instead of (rows, cols, 8).
    - Improved load_mnist by adding fully functioning selection option (will only read what is being asked for).
    - Labels are also optional.

--- amitgroup/features/bedges.py
+++ amitgroup/features/bedges.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from .features import array_bedges
 import numpy as np
 
-def bedges(images, k=6, inflate=True):
+def bedges(images, k=6, inflate=True, edge_first=False):
     """
     Extracts binary edge features for each pixel according to [1].
 
@@ -15,7 +15,9 @@ def bedges(images, k=6, inflate=True):
         There are 6 contrast differences that are checked. The value `k` specifies how many of them must be fulfilled for an edge to be present. The default is all of them (`k` = 6) and gives more conservative edges.
     inflate : bool
         If True, then an edge will appear if any of the 8 neighboring pixels detected an edge. This is equivalent to inflating the edges area with 1 pixel. This adds robustness to your features.
-
+    edgefirst : bool
+        In many cases, it makes more sense to have the features in the shape ``(8, rows, cols)``, which is what this option achieves. Notice that if you process several images, the final shape will be ``(N, 8, rows, cols)``.
+    
     Returns
     -------
     edges : ndarray
@@ -42,8 +44,9 @@ def bedges(images, k=6, inflate=True):
                 feature[:-2,2:] | feature[2:,:-2]
             
     if single:
-        return features[0]
-    else:
-        return features
+        features = features[0]
+    if edgefirst:
+        features = np.rollaxis(features, axis=-1, start=features.ndim-3)
 
+    return features
 
