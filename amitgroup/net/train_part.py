c76b62b840dd503f7838ad3a805ea241b63162aa
Yali Amit <amityali@gmail.com>
2012-10-31 09:42:41 -0500

    Or here

--- amitgroup/net/train_part.py
+++ amitgroup/net/train_part.py
@@ -6,32 +6,76 @@ import train_net as tn
 import pylab as plt
 import pdb
 import amitgroup as ag
+import os
 
 
-def extract_parts(expi):
+def make_synthetic_parts(expi):
+   IM=[]
+   for i in range(expi.pp.numtrain):
+      a=np.zeros((7,7))
+      x=np.floor(np.random.rand()*5)+1
+      horiz=(np.random.rand()<.5)
+      for xi in np.arange(-1,2):
+         for y in range(7):
+            if horiz:
+               a[x+xi,y]=255
+            else:
+               a[y,x+xi]=255
+         
+      im=ag.io.image(0,a)
+      IM.append(im)
 
-   #XY=tn.rearrange(expi.ddtr,0,expi.numtrain)
+   return IM
+         
+
+def extract_parts(expi, data=[]):
+
+   #XY=tn.rearrange(expi.ddtr,0,expi.pp.numtrain)
    #X=XY[0]
    aa=[]
-   for i in range(expi.numtrain):
-      aa.append(ag.io.load_imagep('/home/work/numer/nist/mnist/DEF/VIRT/mnist_train',i,True))
-      feat=ag.features.bedges(double(aa[i].img),5,'box')
-      aa[i].features=feat
-   nt=expi.numtrain
+   sstr=""
+   if (data==[]):
+      sstr=os.environ['HOME']+'/Desktop/Dropbox/mnist_train'
+   else:
+      aa=data
+   for i in range(expi.pp.numtrain):
+       if (sstr!="" and (expi.pp.special_class<0 or ag.io.get_tr(sstr,i)==expi.pp.special_class)):
+          aa.append(ag.io.load_imagep(sstr,i,True))
+          feat=ag.features.bedges(np.double(aa[-1].img),5,'box',expi.pp.spread)
+          #feat=np.ubyte(aa[-1].img>0)
+          #feat.shape=[1,feat.shape[0],feat.shape[1]]
+          aa[-1].features={'V1': feat}
+       else:
+          feat=ag.features.bedges(np.double(aa[i].img),5,'box',expi.pp.spread)
+          aa[i].features={'V1' :feat}
+       
+   nt=len(aa)
+   print 'NT', nt
    num_samps=50
-   ps2=np.floor(expi.pp.part_size/2)
+   lps=expi.pp.part_size
+   if (lps<0):
+      num_samps=1
+      lps=aa[0].img.shape[0]
+      
+   ps2=np.floor(lps/2)
+   md=np.mod(lps,2)
+   print md
    imsizex=aa[0].img.shape[0]
    imsizey=aa[0].img.shape[1]
    ZZ=[]
-   for t in range(nt):
-       ii=np.floor(np.random.rand(num_samps)*(imsizex-expi.pp.part_size))+ps2
-       jj=np.floor(np.random.rand(num_samps)*(imsizey-expi.pp.part_size))+ps2
-       for s in range(num_samps):
-          Z=aa[t].features[:,ii[s]-ps2:ii[s]+ps2+1,jj[s]-ps2:jj[s]+ps2+1]
-          if (np.sum(Z[1:expi.pp.part_size,1:expi.pp.part_size])>expi.pp.min_edges):
-             a=ag.io.image(0,aa[t].img[ii[s]-ps2:ii[s]+ps2+1,jj[s]-ps2:jj[s]+ps2+1],Z)
-             ZZ.append(a)
 
+   for t in range(nt):
+      if (expi.pp.part_size>0):
+         ii=np.floor(np.random.rand(num_samps)*(imsizex-expi.pp.part_size))+ps2
+         jj=np.floor(np.random.rand(num_samps)*(imsizey-expi.pp.part_size))+ps2
+         for s in range(num_samps):
+          Z=aa[t].features['V1'][:,ii[s]-ps2:ii[s]+ps2+md,jj[s]-ps2:jj[s]+ps2+md]
+          if (np.sum(Z[:,1:expi.pp.part_size-1,1:expi.pp.part_size-1])>expi.pp.min_edges):
+             a=ag.io.image(0,aa[t].img[ii[s]-ps2:ii[s]+ps2+md,jj[s]-ps2:jj[s]+ps2+md])
+             a.features={'V1' : Z}
+             ZZ.append(a)
+      else:
+         ZZ.append(aa[t])
    #   pptr=np.array(ZZ)
    
    return(ZZ)
@@ -40,30 +84,33 @@ def extract_parts(expi):
 
 
                 
-def train_parts(expi):
+def train_parts(expi, data=[]):
 
     # Extract random windows from a number of training images
-    ZZ=extract_parts(expi)
+    ZZ=extract_parts(expi,data)
     print 'No of windows ', len(ZZ)
     raw_input()
     Jmid=expi.pp.Jmax/2
     # Initial value under 1 of each synapse. Is this necessary?
     Jqtr=Jmid*expi.pp.reduction_factor
     numtrain=len(ZZ)
-    numfeat=ZZ[0].features.size
+    numfeat=ZZ[0].features['V1'].size
     TT=range(numtrain)
 
     # List of learned parts
     Parts=[]
+    totpot=0
+    totdep=0
     for it in range(expi.pp.numit):
         print 'iteration ', it, len(Parts)
-        raw_input()
+        #raw_input()
         np.random.shuffle(TT)
         inp=[]
+        tc=0
         for t in TT:
             hits=0
             # Find on features, those synapses can modify.
-            XI=(ZZ[t].features.flatten())==1
+            XI=(ZZ[t].features['V1'].flatten())==1
             #XI.shape=[numfeat,1]
             i=0
             numparts=len(Parts)            
@@ -73,41 +120,73 @@ def train_parts(expi):
                #print Parray
                Parray.shape=[numparts,numfeat]
                # Multiple part array by features of this image.
-               H=np.dot(Parray,ZZ[t].features.flatten())
-               #print H
+               H=np.dot(Parray,ZZ[t].features['V1'].flatten())
+               if (expi.pp.showing==1):
+                  print 'Fields', H
                #raw_input()
                # Find best fit part.
                hi=np.argmax(H,0)
                # This list will be depressed.
-               di=np.where(H<expi.pp.theta)[0]
+               di=np.where(np.logical_and(H<expi.pp.theta,H>expi.pp.theta-expi.pp.deltaD))[0]
+               if (expi.pp.showing==1):
+                  print di
                # If best fit above threshold potentiate
                if (H[hi]>expi.pp.theta):
-                  hits+=np.sum(H[hi]>expi.pp.theta)
-                  tn.potentiate_ff(expi.pp,H[hi],XI,Parts[hi],Jmid)
+                  hits=1
+                  s=tn.potentiate_ff(expi.pp,H[hi],XI,Parts[hi],Jmid)
+                  totpot+=s
+                  if (expi.pp.showing==1):
+                     print 'pot ', s
                # Depress all other models?
-               else:
-                  for dii in range(di.size):
-                     tn.depress_ff(expi.pp,H[di[dii]],XI,Parts[di[dii]],Jmid)
+               s=0
+               for dii in range(di.size):
+                  s+=tn.depress_ff(expi.pp,H[di[dii]],XI,Parts[di[dii]],Jmid)
+                  totdep+=s
+               if (expi.pp.showing==1):
+                  print 'dep ', s
             # No part model fit this data point - start new part model with potentiation
             if (hits==0):
                J=np.ones(numfeat)*Jqtr
-               h=np.dot(ZZ[t].features.flatten(),J-Jmid)
-               #print 'h before ', h
-               tn.potentiate_ff(expi.pp,h,XI,J,Jmid)
-               #print 'h after ', np.dot(ZZ[t].features.flatten(),J-Jmid)
+               h=np.dot(ZZ[t].features['V1'].flatten(),J-Jmid)
+               if (expi.pp.showing==1):
+                  print 'h before ', h
+               # Learn until field is positive.
+               w=0
+               while (h<=expi.pp.theta):
+                  w+=1
+                  tn.potentiate_ff(expi.pp,h,XI,J,Jmid)
+                  h=np.dot(ZZ[t].features['V1'].flatten(),J-Jmid)
+               if (expi.pp.showing==1):
+                  print 'h after ', w, 'iterations ', np.dot(ZZ[t].features['V1'].flatten(),J-Jmid)
                Parts.append(J)
-               print t, len(Parts)
+
+            if (expi.pp.showing==1):
+               print tc, t, len(Parts)
+               [ip,Parray,H]=get_best_part(Parts,ZZ,Jmid)
+               print H
+               print 'Parray'
+               print Parray
+               raw_input()
+            tc+=1
         # Let's get rid of parts that have no takers
-        Parray=np.array(Parts)
-        #print Parray
-        Parray.shape=[len(Parts),numfeat]
-        pptr,ims=stack_features(ZZ)
-        H=np.dot(Parray-Jmid,transpose(pptr))
-        ip=argmax(H,0)
-        iip=unique(ip)
+        [ip,Parray,H]=get_best_part(Parts,ZZ, Jmid)
+        bef_len=len(Parts)
+        lip=np.unique(ip,True)
+        if (expi.pp.showing==1):
+           print 'list of parts ', lip[0]
+           print 'list of indices ', lip[1]
+           #raw_input()
+        lipa=np.zeros(lip[1].size+1)
+        lipa[0:lip[1].size]=lip[1]
+        lipa[lip[1].size]=ip.size-1
+        dlip=np.where(lipa[1:lipa.size]-lipa[0:lipa.size-1]>1)
+        iip=lip[0][dlip]
+        if (expi.pp.showing==1):
+           print 'list of surviving parts that had more than one element', iip
+        #  iip=np.unique(ip)
         Parts=[]
         Parts=list(Parray[iip,:])
-        print 'After pruning num parts ',len(Parts)
+        print 'Before pruning ', bef_len, 'After pruning num parts ',len(Parts), 'Pot ', totpot, 'Dep ', totdep
     print 'Final number of parts ', len(Parts)
     n=1
     pp=len(Parts)
@@ -117,49 +196,59 @@ def train_parts(expi):
 
 # I am making this change to get it onto the mac
 def stack_features(ZZ):
-   numfeat=ZZ[0].features.size
+   numfeat=ZZ[0].features['V1'].size
    pptr=np.zeros((len(ZZ),numfeat))
    ims=np.zeros((len(ZZ),ZZ[0].img.shape[0],ZZ[0].img.shape[1]))
    for i in range(len(ZZ)):
       ims[i,:,:]=ZZ[i].img
-      pptr[i,:]=ZZ[i].features.flatten()
+      pptr[i,:]=ZZ[i].features['V1'].flatten()
 
    return pptr, ims
 
+def get_best_part(Parts, data, Jmid):
+   Parray=np.array(Parts)
+   #print Parray
+   numfeat=data[0].features['V1'].size
+   Parray.shape=[len(Parts),numfeat]
+   pptr,ims=stack_features(data)
+   H=np.dot(Parray-Jmid,np.transpose(pptr))
+   print H.shape
+   ip=np.argmax(H,0)
+   print ip
+   ip=np.sort(ip)
+   return ip, Parray, H
+
 def show_clusters(expi,ZZ,Parts):
 
-   
+   plt.close('all')
    nump=len(Parts)
    numparts=len(Parts)
-   numfeat=ZZ[0].features.size
+   numfeat=ZZ[0].features['V1'].size
    pptr,ims=stack_features(ZZ)
-
-   TT=np.array(Parts)
-   TT.shape=[numparts,numfeat]
    Jmid=expi.pp.Jmax/2
-   H=np.dot(TT-Jmid,transpose(pptr))
-   print H
-   raw_input()
-   imax=np.argmax(H,0);
-   print imax.shape, H.shape
+   [imax,pp,H]=get_best_part(Parts,ZZ,Jmid)
+   print imax.shape
    MM=np.zeros((numparts,numfeat))
    n=1
    num_images_per_row=20
-   num_rows=int(ceil(numparts/20.))
+   num_rows=2*int(np.ceil(numparts/20.))
+   num_type=pptr[0,:].size/ims[0,:].size
    for ip in range(numparts):
       print 'size of cluster ', np.sum(imax==ip)
 
       if (np.sum(imax==ip)>0):
-         Hm=np.mean(pptr[imax==ip,:],0).flatten()
-         Im=np.mean(ims[imax==ip,:,:],0)
-         MM[ip,:]=Hm
-         G=MM[ip,:]
-         G.shape=[8,expi.pp.part_size,expi.pp.part_size]
-        # for a in range(8):
+        
+         Im=np.mean(ims[imax==ip,:,:],0)      
          plt.subplot(num_rows,num_images_per_row,n)
+         plt.imshow(Im,cmap=plt.get_cmap('gray'))
+         plt.axis('off')
+         plt.subplot(num_rows,num_images_per_row,n+num_rows*10)
+         pt=Parts[ip]
+         pt.shape=[num_type,Im.shape[0],Im.shape[1]]
+         plt.imshow(pt[0,:,:], cmap=plt.get_cmap('gray'))
+         plt.axis('off')
          n=n+1
-         plt.imshow(Im,cmap=get_cmap('gray'))
-         #imshow(G[a,:,:], cmap=get_cmap('gray'))
-         axis('off')
-         
+
+        
+   plt.hold(False)
       
