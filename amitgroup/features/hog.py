1e5d18c358df51a1fcdfd1a59d355322ab4becfa
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-11-02 15:21:12 -0500

    Started implementing blocks for HOG.

--- amitgroup/features/hog.py
+++ amitgroup/features/hog.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import scipy.signal as sig
+import scipy.stats
 from itertools import product
 import math
 
@@ -40,5 +41,24 @@ def hog(data, cell_size, block_size, num_bins=9):
                     votes[x+1,y,angle_bin] += kernels[1,i,j] * v 
                     votes[x,y+1,angle_bin] += kernels[2,i,j] * v
                     votes[x+1,y+1,angle_bin] += kernels[3,i,j] * v
+
+    # Now, sum these up over blocks and normalize
+    blocks_shape = tuple([cells_shape[i]-block_size[i]+1 for i in xrange(2)])
+
+    features = np.zeros(blocks_shape + (num_bins,))
+
+    eps2 = 0.01
+
+    # The minus one is important for centering the gaussian
+    w, h = (blocks_shape[0]-1)/2, (blocks_shape[1]-1)/2
+    for x in xrange(blocks_shape[0]):
+        for y in xrange(blocks_shape[1]):
+            v = np.zeros(votes.shape[-1])
+            for i in xrange(block_size[0]):
+                for j in xrange(block_size[1]):
+                    v += votes[x+i,y+j] * scipy.stats.norm.pdf(np.sqrt((x-w)**2 + (y-h)**2), scale=0.5 * block_size[0])
+
+            # Normalize and put into features 
+            features[x,y] = v / np.sqrt(np.dot(v, v) + eps2)
             
-    return votes
+    return features 
--- examples/hog_example.py
+++ examples/hog_example.py
@@ -3,14 +3,14 @@ import amitgroup as ag
 import matplotlib.pylab as plt
 import numpy as np
 
-data = plt.imread('circle.png')
-data = data[...,:3].mean(axis=-1)
+origdata = plt.imread('circle.png')
+data = origdata[...,:3].mean(axis=-1)
 
 
-feat = ag.features.hog(data, (6, 6), (2, 2), num_bins=12)
+feat = ag.features.hog(data, (6, 6), (3, 3), num_bins=12)
 
 plt.subplot(121)
-plt.imshow(data)
+plt.imshow(origdata)
 plt.subplot(122)
-ag.plot.visualize_hog(feat, (21, 21), show=False)
+ag.plot.visualize_hog(feat, (5, 5), show=False)
 plt.show()
