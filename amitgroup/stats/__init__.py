e98b7314bcfa0fb6d773a3ac61ebb1b07daaa7bd
Gustav Larsson <gustav.m.larsson@gmail.com>
2013-02-15 18:19:07 -0600

    Removed import to old sparse bernoull mixture.

--- amitgroup/stats/__init__.py
+++ amitgroup/stats/__init__.py
@@ -1,6 +1,5 @@
 
 from __future__ import absolute_import
 from .bernoulli_mixture import BernoulliMixture
-from .bernoulli_mixture_sparse import BernoulliMixtureSparse
 from .image_deformation import image_deformation
 from .bernoulli_deformation import bernoulli_deformation 
