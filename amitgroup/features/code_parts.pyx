8739ca9376bade2b081e7be5303af5381c7f2417
Gustav Larsson <gustav.m.larsson@gmail.com>
2013-11-17 15:43:02 -0600

    Testing new parts threshold (log ratio against homogenized parts).

--- amitgroup/features/code_parts.pyx
+++ amitgroup/features/code_parts.pyx
@@ -310,13 +310,145 @@ def code_parts_new(np.ndarray[ndim=3,dtype=UINT_t] X,
 
     return out_map
 
+def code_parts_mmm(np.ndarray[ndim=3,dtype=UINT_t] X,
+                   np.ndarray[ndim=3,dtype=UINT_t] X_unspread,
+                   np.ndarray[ndim=4,dtype=np.float64_t] log_parts,
+                   np.ndarray[ndim=4,dtype=np.float64_t] log_invparts,
+                   int threshold, outer_frame=0, int collapse=1, accept_threshold=1.0):
+    """
+    At each location of `X`, find the log probabilities for each part and location. Outputs these part assignments in the same data dimensions as `X`. Neighborhoods of `X` with edge counts lower than `threshold` are regarded as background and assigned zero.
+
+    Parameters
+    ----------
+    X : ndarray[ndim=3,dtype=np.uint8]
+        The first two dimensions of the array specify locations. The last one specifies a binary edge type. The value ``X[s,t,e]`` is 1 iff there is an edge of type `e` detected at location `(s,t)`.
+    log_parts : ndarray[ndim=4]
+        We have a Bernoulli mixture model defined over patches of the input image. The `log_parts` is a logarithm applied to the array of edge probability maps for each part. Array of shape `(K, S, T, E)`, where `K` is the number of mixture component, `S` and `T` the shape of the data, and `E` the number of edges. The value of ``log_parts[k,s,t,e]`` is the log probability of observing an edge `e` at location `(s,t)`, conditioned on the mixture component being `k`.
+    log_invparts : ndarray[ndim=4]
+        Preprocessed inverse of `log_parts`, i.e. ``log(1-exp(log_parts))``.
+    threshold : int
+        The least number of edges in a patch to reject the null background hypothesis.
+    outer_frame : int
+        Remove a frame of this thickness when checking the threshold. If the parts are 5 x 5, and this is set to 1, then only the center 3 x 3 is used to count edges when compared to the threshold. 
+    
+    Returns
+    -------
+    out_map : ndarray[ndim=3] 
+        Array of shape `(S, T, K+1)`. There are two cases, either the third dimension is `(0, -inf, -inf, ...)`, when there are insufficient edges in the neighborhood of a location. Otherwise, `out_map[s,t,i+1]` is the log likelihood of part `i` at location `(s,t)`. Additionally, `out_map[s,t,0]` is equal to `-inf`.
+    """
+
+    cdef unsigned int num_parts = log_parts.shape[0]
+    cdef unsigned int part_x_dim = log_parts.shape[1]
+    cdef unsigned int part_y_dim = log_parts.shape[2]
+    cdef unsigned int part_z_dim = log_parts.shape[3]
+    cdef unsigned int X_x_dim = X.shape[0]
+    cdef unsigned int X_y_dim = X.shape[1]
+    cdef unsigned int X_z_dim = X.shape[2]
+    cdef unsigned int new_x_dim = X_x_dim - part_x_dim + 1
+    cdef unsigned int new_y_dim = X_y_dim - part_y_dim + 1
+    cdef unsigned int i_start,j_start,i_end,j_end,count,i,j,z,k, cx0, cx1, cy0, cy1 
+    cdef unsigned int i_frame = <unsigned int>outer_frame
+    cdef DTYPE_t NINF = DTYPE(-np.inf)
+    # we have num_parts + 1 because we are also including some regions as being
+    # thresholded due to there not being enough edges
+    
+    cdef np.ndarray[dtype=np.int32_t, ndim=2] out_map = -np.ones((new_x_dim,
+                                                                 new_y_dim), dtype=np.int32)
+    cdef np.ndarray[dtype=DTYPE_t, ndim=1] vs = np.ones(num_parts, dtype=DTYPE)
+    cdef DTYPE_t[:] vs_mv = vs
+
+    cdef np.ndarray[dtype=DTYPE_t, ndim=1] vs_alt = np.ones(num_parts, dtype=DTYPE)
+    cdef DTYPE_t[:] vs_alt_mv = vs_alt
+
+    parts = np.exp(log_parts)
+    parts_alt = np.tile(np.apply_over_axes(np.mean, parts, [1, 2]), (part_x_dim, part_y_dim, 1))
+    #parts_alt = np.tile(np.apply_over_axes(np.mean, parts, [1, 2, 3]), (part_x_dim, part_y_dim, part_z_dim))
+
+    cdef np.ndarray[dtype=DTYPE_t, ndim=4] part_logits_alt = np.rollaxis((np.log(parts_alt) - np.log(1 - parts_alt)).astype(DTYPE), 0, 4).copy()
+    cdef np.ndarray[dtype=DTYPE_t, ndim=1] constant_terms_alt = np.apply_over_axes(np.sum, np.log(1 - parts_alt).astype(DTYPE), [1, 2, 3]).ravel()
+
+    cdef DTYPE_t[:,:,:,:] part_logits_alt_mv = part_logits_alt
+    cdef DTYPE_t[:] constant_terms_alt_mv = constant_terms_alt
+
+    cdef UINT_t[:,:,:] X_mv = X
+    cdef UINT_t[:,:,:] X_unspread_mv = X_unspread
+
+    cdef np.ndarray[dtype=DTYPE_t, ndim=4] part_logits = np.rollaxis((log_parts - log_invparts).astype(DTYPE), 0, 4).copy()
+
+    cdef np.ndarray[dtype=DTYPE_t, ndim=1] constant_terms = np.apply_over_axes(np.sum, log_invparts.astype(DTYPE), [1, 2, 3]).ravel()
+    cdef DTYPE_t[:,:,:,:] part_logits_mv = part_logits
+    cdef DTYPE_t[:] constant_terms_mv = constant_terms
+
+    cdef np.int32_t[:,:] out_map_mv = out_map
+
+    cdef np.ndarray[dtype=UINT32_t, ndim=2] _integral_counts = np.zeros((X_x_dim+1, X_y_dim+1), dtype=UINT32)
+    cdef UINT32_t[:,:] integral_counts = _integral_counts
+
+    cdef DTYPE_t max_value, v
+    cdef int max_index 
+
+    # The first cell along the num_parts+1 axis contains a value that is either 0
+    # if the area is deemed to have too few edges or min_val if there are sufficiently many
+    # edges, min_val is just meant to be less than the value of the other cells
+    # so when we pick the most likely part it won't be chosen
+
+    # Build integral image of edge counts
+    # First, fill it with edge counts and accmulate across
+    # one axis.
+    for i in range(X_x_dim):
+        for j in range(X_y_dim):
+            count = 0
+            for z in range(X_z_dim):
+                count += X_unspread_mv[i,j,z]
+            integral_counts[1+i,1+j] = integral_counts[1+i,j] + count
+    # Now accumulate the other axis
+    for j in range(X_y_dim):
+        for i in range(X_x_dim):
+            integral_counts[1+i,1+j] += integral_counts[i,1+j]
+
+    # Code parts
+    for i_start in range(X_x_dim-part_x_dim+1):
+        i_end = i_start + part_x_dim
+        for j_start in range(X_y_dim-part_y_dim+1):
+            j_end = j_start + part_y_dim
+
+            # Note, integral_counts is 1-based, to allow for a zero row/column at the zero:th index.
+            cx0 = i_start+i_frame
+            cx1 = i_end-i_frame
+            cy0 = j_start+i_frame
+            cy1 = j_end-i_frame
+            count = integral_counts[cx1, cy1] - \
+                    integral_counts[cx0, cy1] - \
+                    integral_counts[cx1, cy0] + \
+                    integral_counts[cx0, cy0]
+
+            if threshold <= count:
+                vs[:] = constant_terms
+                vs_alt[:] = constant_terms_alt
+                with nogil:
+                    for i in range(part_x_dim):
+                        for j in range(part_y_dim):
+                            for z in range(X_z_dim):
+                                if X_mv[i_start+i,j_start+j,z]:
+                                    for k in range(num_parts):
+                                        vs_mv[k] += part_logits_mv[i,j,z,k]
+                                        vs_alt_mv[k] += part_logits_alt_mv[i,j,z,k]
+
+                max_index = vs.argmax()
+                if vs_mv[max_index] - vs_alt_mv[max_index] >= accept_threshold:
+                    out_map_mv[i_start,j_start] = max_index / collapse
+                
+                #out_map_mv[i_start,j_start] = vs.argmax() / collapse
+
+    return out_map
+
 def subsample_offset_shape(shape, size):
     return [int(shape[i]%size[i]/2 + size[i]/2)  for i in xrange(2)]
 
-def extract_parts(edges, unspread_edges, log_parts, log_invparts, int threshold, outer_frame=0, spread_radii=(4, 4), subsample_size=(4, 4), int collapse=1):
+def extract_parts(edges, unspread_edges, log_parts, log_invparts, int threshold, outer_frame=0, spread_radii=(4, 4), subsample_size=(4, 4), int collapse=1, accept_threshold=0.0):
     cdef:
         int num_feats = log_parts.shape[0]//collapse
-        np.ndarray[np.int32_t,ndim=2] parts = code_parts_new(edges, unspread_edges, log_parts, log_invparts, threshold, outer_frame=outer_frame, collapse=collapse)
+        np.ndarray[np.int32_t,ndim=2] parts = code_parts_mmm(edges, unspread_edges, log_parts, log_invparts, threshold, outer_frame=outer_frame, collapse=collapse, accept_threshold=accept_threshold)
         np.ndarray[np.uint8_t,ndim=3] feats = np.zeros((parts.shape[0]//subsample_size[0],
                                                         parts.shape[1]//subsample_size[1],
                                                         num_feats),
