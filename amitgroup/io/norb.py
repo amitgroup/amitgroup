53f867ebef17f430b24ea3205129cbb0e66ce4ed
Gustav Larsson <gustav.m.larsson@gmail.com>
2014-05-17 12:13:36 -0500

    Added docstring to ag.io.load_small_norb.

--- amitgroup/io/norb.py
+++ amitgroup/io/norb.py
@@ -1,11 +1,26 @@
 from __future__ import division, print_function, absolute_import
-
 import os
 import struct
 import numpy as np
 
-
 def load_small_norb(dataset, norb_path=None):
+    """
+    Loads the small NORB dataset [1].
+
+    Parameters
+    ---------- 
+    dataset : ('training', 'testing')
+        Select between training or testing.
+    norb_path : str
+        Specifies the path in which you have your NORB files. Default is None, in which case the environment variable ``NORB_DIR`` is used.
+
+    Returns
+    -------
+    X : ndarray
+        The images
+    y : ndarray
+        The labels
+    """
     assert dataset in ('training', 'testing')
 
     if norb_path is None:
@@ -34,6 +49,7 @@ def load_small_norb(dataset, norb_path=None):
         assert byte_matrix == 0x1e3d4c54, "Only supports intger matrix for now"
 
         ndim = struct.unpack('<I', f.read(4))[0]
+        # The two latter values are ignored as per specifications
         dim0 = struct.unpack('<III', f.read(4 * 3))[0]
 
         y = np.fromfile(f, dtype=np.int32, count=dim0)
