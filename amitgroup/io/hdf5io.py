356c21ddf25cd2d76686c3b9da2a76a472d51880
Gustav Larsson <gustav.m.larsson@gmail.com>
2014-08-06 14:01:44 -0500

    Updated to using PyTables newer API.
    This means some older versions of PyTables are no longer supported.

--- amitgroup/io/hdf5io.py
+++ amitgroup/io/hdf5io.py
@@ -1,29 +1,34 @@
 import numpy as np
 import tables
+import warnings
 
 # Types that should be saved as pytables attribute
-ATTR_TYPES = (int, float, bool, str, 
-              np.int8, np.int16, np.int32, np.int64, 
+ATTR_TYPES = (int, float, bool, str,
+              np.int8, np.int16, np.int32, np.int64,
               np.uint8, np.uint16, np.uint32, np.uint64,
               np.float16, np.float32, np.float64,
               np.bool_, np.complex64, np.complex128)
 
 try:
     COMPRESSION = tables.Filters(complevel=9, complib='blosc', shuffle=True)
-except Exception: #type?
+except Exception:
     warnings.warn("Missing BLOSC; no compression will used.")
     COMPRESSION = tables.Filters()
+
 
 def _save_level(handler, group, level, name=None):
     if isinstance(level, dict):
         # First create a new group
-        new_group = handler.createGroup(group, name, "dict:{}".format(len(level)))
+        new_group = handler.create_group(group, name,
+                                        "dict:{}".format(len(level)))
         for k, v in level.items():
-            _save_level(handler, new_group, v, name=k) 
+            _save_level(handler, new_group, v, name=k)
     elif isinstance(level, list):
-        # Lists can contain other dictionaries and numpy arrays, so we don't want to
-        # serialize them. Instead, we will store each entry as i0, i1, etc.
-        new_group = handler.createGroup(group, name, "list:{}".format(len(level)))
+        # Lists can contain other dictionaries and numpy arrays, so we don't
+        # want to serialize them. Instead, we will store each entry as i0, i1,
+        # etc.
+        new_group = handler.create_group(group, name,
+                                        "list:{}".format(len(level)))
 
         for i, entry in enumerate(level):
             level_name = 'i{}'.format(i)
@@ -31,20 +36,22 @@ def _save_level(handler, group, level, name=None):
 
     elif isinstance(level, np.ndarray):
         atom = tables.Atom.from_dtype(level.dtype)
-        node = handler.createCArray(group, name, atom=atom, shape=level.shape, chunkshape=level.shape, filters=COMPRESSION) 
+        node = handler.create_carray(group, name, atom=atom, shape=level.shape,
+                                    chunkshape=level.shape,
+                                    filters=COMPRESSION)
         node[:] = level
     elif isinstance(level, ATTR_TYPES):
         setattr(group._v_attrs, name, level)
     else:
-        node = handler.createVLArray(group, name, tables.ObjectAtom())
+        node = handler.create_vlarray(group, name, tables.ObjectAtom())
         node.append(level)
-        
+
 
 def _load_level(level):
     if isinstance(level, tables.Group):
-        dct = {} 
+        dct = {}
         # Load sub-groups
-        for grp in level: 
+        for grp in level:
             dct[grp._v_name] = _load_level(grp)
 
         # Load attributes
@@ -62,10 +69,11 @@ def _load_level(level):
     elif isinstance(level, tables.VLArray):
         if level.shape == (1,):
             return level[0]
-        else: 
+        else:
             return level[:]
     elif isinstance(level, tables.Array):
         return level[:]
+
 
 def save(path, data):
     """
@@ -85,13 +93,13 @@ def save(path, data):
     * Numpy arrays
 
     A recommendation is to always convert your data to using only these four
-    ingredients. That way your data will always be retrievable by any HDF5 
+    ingredients. That way your data will always be retrievable by any HDF5
     reader. A class that helps you with this is `amitgroup.util.Saveable`.
 
     This function requires the [PyTables] module to be installed.
 
     Parameters
-    ---------- 
+    ----------
     path : file-like object or string
         File or filename to which the data is saved.
     data : anything
@@ -101,17 +109,17 @@ def save(path, data):
 
     See also
     --------
-    load 
+    load
 
     """
     if not isinstance(path, str):
         path = path.name
 
-    h5file = tables.openFile(path, mode='w', title='Test file')
-    #group = h5file.createGroup('/', 'detector', 'Detector information')
+    h5file = tables.open_file(path, mode='w', title='Test file')
     group = h5file.root
     _save_level(h5file, group, data, name='top')
     h5file.close()
+
 
 def load(path):
     """
@@ -120,11 +128,11 @@ def load(path):
     This function requires the [PyTables] module to be installed.
 
     Parameters
-    ---------- 
+    ----------
     path : file-like object or string
-        File or filename from which to load the data. 
+        File or filename from which to load the data.
 
-    Returns 
+    Returns
     --------
     data : anything
         Hopefully an identical reconstruction of the data that was saved.
@@ -137,7 +145,7 @@ def load(path):
     if not isinstance(path, str):
         path = path.name
 
-    h5file = tables.openFile(path, mode='r')
+    h5file = tables.open_file(path, mode='r')
     data = _load_level(h5file.root)['top']
     h5file.close()
-    return data 
+    return data
