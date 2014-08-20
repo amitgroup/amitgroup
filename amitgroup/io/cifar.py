2098c9a5752f3a0f3cb835df5757c1646dceb33a
Gustav Larsson <gustav.m.larsson@gmail.com>
2014-08-20 17:30:40 -0500

    Updates to CIFAR-10 loader.

--- amitgroup/io/cifar.py
+++ amitgroup/io/cifar.py
@@ -4,7 +4,8 @@ import numpy as np
 import os
 
 
-def load_cifar_10(section, offset=0, count=10000):
+def load_cifar_10(section, offset=0, count=10000, x_dtype=np.float32,
+                  ret='xy'):
     assert section in ['training', 'testing']
 
     # TODO: This loads it from hdf5 files that I have prepared.
@@ -18,17 +19,43 @@ def load_cifar_10(section, offset=0, count=10000):
 
     if section == 'training':
         batch_number = offset // 10000 + 1
-        assert 1 <= batch_number <= 5
-        name = 'cifar_{}.h5'.format(batch_number)
-        batch_offset = offset % 10000
+        #assert 1 <= batch_number <= 5
+        if batch_number > 5:
+            name = None
+            batch_offset = 0
+            count = 0
+        else:
+            name = 'cifar_{}.h5'.format(batch_number)
+            batch_offset = offset % 10000
     else:
         name = 'cifar_test.h5'
+        batch_offset = offset
 
-    data = ag.io.load(os.path.join(os.environ['CIFAR10_DIR'], name))
+    if name is not None:
+        data = ag.io.load(os.path.join(os.environ['CIFAR10_DIR'], name))
+    else:
+        data = dict(data=np.empty(0), labels=np.empty(0))
 
     X = data['data']
     y = data['labels']
 
-    # TODO: This has to read the whole batch
-    return (X[batch_offset:batch_offset+count].reshape(-1, 3, 32, 32),
-            y[batch_offset:batch_offset+count])
+    X0 = X[batch_offset:batch_offset+count].reshape(-1, 3, 32, 32)
+    y0 = y[batch_offset:batch_offset+count]
+
+    if x_dtype in [np.float16, np.float32, np.float64]:
+        X0 = X0.astype(x_dtype) / 255
+    elif x_dtype == np.uint8:
+        pass  # Do nothing
+    else:
+        raise ValueError('load_cifar_10: Unsupported value for x_dtype')
+
+    returns = []
+    if 'x' in ret:
+        returns.append(X0)
+    if 'y' in ret:
+        returns.append(y0)
+
+    if len(returns) == 1:
+        return returns[0]
+    else:
+        return tuple(returns)
